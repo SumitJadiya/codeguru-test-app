a404ff8b245daaadebd86d5b0b29b2c36c62ba76
Sumit Jadiya <sumit.jadiya@turtlemint.com>
2025-04-03 14:34:44 +0530

    init

---
+++ src/test/java/com/codeguru/review/ReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeguru.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
